b21a687ec96d7f14a8646b7e96bfb16287587576
Ojus <ojus.save@zoom.us>
2024-07-23 19:03:59 -0700

    updated auth.js

---
+++ auth.js
@@ -0,0 +1,10 @@
+const config = {
+    accountId: '',
+    clientId: '',
+    clientSecret: '',
+    zoomApiBaseUrl: '',
+    endpointUrl: 'https://your-endpoint-url.com/api' // Add your endpoint URL here
+  };
+  
+  module.exports = config;
+  
